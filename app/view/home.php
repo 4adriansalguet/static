af4bc70ec6933dde4ed7fe7ec47623809683e2b4
webdesign day <a.adriansalguet3.code@gmail.com>
2023-03-31 15:17:47 +0800

    new edit

--- app/view/home.php
+++ app/view/home.php
@@ -1,14 +1,25 @@
+<section id="section_2">
+			<div class="section_2_content">
+				<h2>Make Garcia Painting your one and only choice.</h2>
+				<p>NO JOB TOO SMALL! WE DO WHAT THE OTHER GUYS WON’T!</p>
+				<div class="section_2_btn_container">
+					<a href="#">
+                    <img src="public/images/common/call_button.jpg" alt="">
+					</a>
+				</div>
+			</div>
+		</section>
 
     <section class="___section_3">
         <div class="___section_3_container">
-            <h3 class="services_after">WHAT WE DO</h3>
+            <h3>WHAT WE DO</h3>
             <h2><span>Our</span> Services</h2>
             <div class="___section_3_flex">
 
                 <div class="__section_3_card __section_3_card_bg_1">
                     <div class="__section_3_card_body">
                         <h2>Interior Painting</h2>
-                        <a href="#">LEARN MORE</a>
+                        <a href="#" class="btn">LEARN MORE</a>
                     </div>
                 </div>
 
@@ -25,89 +36,87 @@
                         <a href="#">LEARN MORE</a>
                     </div>
                 </div>
-
             </div>
         </div>
     </section>
 
 	<section id="section_4">
 
-		<section class="kitchen_section">
-        <div class="kitchen-container">
-			<img src="./public/images/gallery/kitchen.fw.png" alt="">
+		<div class="kitchen-container">
+			<img src="public/images/common/kitchen.jpg" alt="">
 			<div class="kitchen-des">
 				<h3>WELCOME TO</h3>
 				<h4 class="title">GARCIA PAINTING</h4>
 				<P class="sub-title">A TERRIFIC INVESTMENT</P>
 				<p>
                 A new coat of paint adds value and protection to any kind of property, 
-                whether that property is commercial, residential or industrial. <br> <br>  
+                whether that property is commercial, residential or industrial. 
+                </p>
+                
+                <p>
                 However, the value of that property will only be enhanced by a good paint job, 
                 the kind that’s provided by professionals like Garcia Painting.
                 </p>
-				<a href="#">LEARN MORE</a>
+				<a href="#" class="btn">LEARN MORE</a>
 			</div>
 		</div>
-        </section>
 
-		<section class="wall_section">
-        <div class="wall-container">
+		<div class="wall-container">
 			<div class="wall-des">
 			<h3 class="title">Extensive Experience</h3>
             <p>
             Established in 2008, we’ve got extensive experience and the elite expertise that comes along with it. 
-            Let us be your one and only choice for all sorts of painting projects. <br> <br>
-            As a locally owned company, we are more than happy to work with fellow members of the Gilroy community.
-            </p>            
-				<a class="sub-title" href="#">LEARN MORE</a>
-			</div>
-			<img src="./public/images/gallery/housefw.png" alt="">
-		</div>
-        </section>
+            Let us be your one and only choice for all sorts of painting projects. 
+            </p>
 
-        <section class="house_yard">
+            <p>
+            As a locally owned company, we are more than happy to work with fellow members of the Gilroy community.
+            </p>
+            
+				<a href="#" class="btn">LEARN MORE</a>
+			</div>
+			<img src="public/images/common/wall.jpg" alt="">
+		</div>
+
         <div class="kitchen-container">
-			<img src="./public/images/gallery/house4fw.png" alt="">
+			<img src="public/images/common/lawn.jpg" alt="">
 			<div class="kitchen-des">
 				<!-- <h3>WELCOME TO</h3> -->
-				<h4 class="title gal-col3">Restore and Renovate</h4>
+				<h4 class="title">Restore and Renovate</h4>
 				<!-- <P class="sub-title">A TERRIFIC INVESTMENT</P> -->
 				<p>
-                At Garcia painting, we work to restore, renovate and create 
-                the environment that best suits your lifestyle. <br> <br>
-                We are committed to professionalism, creative ideas with color and design,
-                 respect for your property and time, and a pleasant process with a job well done.
+                At Garcia painting, we work to restore, renovate and create the environment that best suits your lifestyle.
                 </p>
-				<a href="#">LEARN MORE</a>
+
+                <p>
+                We are committed to professionalism, creative ideas with color and design, respect for your property and time, and a pleasant process with a job well done.
+                </p>
+				<a href="#" class="btn">LEARN MORE</a>
 			</div>
 		</div>
-        </section>
-        
 	</section>
 
      <section id="gallery_section">
             <h3>WHAT WE DO</h3>
             <p><span>Our</span> Gallery</p>
-
             <div class="gallery_container">
-            <div class="pictures_left">  
-                <img src="./public/images/icons/dining.fw.png" alt="">
-                <img src="./public/images/icons/outdoor.fw.png" alt="">
-                <div class="pkyu">
-                    <img src="./public/images/icons/lounge.fw.png" alt="">
+            <!-- <div class="pictures_left">  
+                <img src="public/images/common/kitchen2.jpg" alt="">
+                <img src="public/images/common/labas.jpg" alt="">
+                <div>
+                <img class="lounge" src="public/images/common/sala.jpg" alt="">
                 </div>
             </div>
 
             <div class="pictures_right">
-                <img class="suburbs" src="./public/images/icons/suburbs.fw.png" alt="">
+                <img class="suburb" src="public/images/common/labas2.jpg" alt="">
                 <div class="strp_subr">
-                <img class="stripe" src="./public/images/icons/stripehouse.fw.png" alt="">
-                <img  class="sala" src="./public/images/icons/salaset.fw.png  " alt="">
+                <img class="stripe" src="public/images/common/stripe.jpg" alt="">
+                <img  class="sala" src="public/images/common/sala2.jpg" alt="">
                 </div>
-            </div>
+            </div> -->
             
             </div>
-            
     </section> 
 
 
--- app/view/template/header.php
+++ app/view/template/header.php
@@ -6,7 +6,9 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- bawal -->
 	<meta name="apple-mobile-web-app-capable" content="yes" /> <!-- bawal --> 
 	<meta name="apple-mobile-web-app-status-bar-style" content="black" /> <!-- bawal --> 
-
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
 	<?php $this->helpers->seo($view); ?> <!-- bawal -->
 	<link rel="icon" href="public/images/favicon.png" type="image/x-icon"> <!-- bawal -->
 	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet"> <!-- bawal -->
@@ -30,7 +32,7 @@
 					<img src="./public/images/icons/socmed.fw.png" alt="">
 					<a href="#">CALL US TODAY</a>
 					<img src="./public/images/icons/phone.fw.png" alt="">
-					<a href="#"></a>
+					<img src="" alt="">
 					<p class="tel">(408) 413-9093</p>
 				</div>
 			</div>
@@ -40,30 +42,32 @@
 			<div class="home-main">
 				<div class="home-content">
 					<img src="./public/images/icons/logo.fw.png" alt="">
-					<a class="button" href="#">HOME</a>
+					<nav>
+						<a href="#" id="pull">MENU</a>
+						<ul>
+			                <li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a  href="home#content">HOME</a></li>
+			                <li <?php $this->helpers->isActiveMenu("about"); ?>><a href="about#content">ABOUT US</a></li>
+			                <li <?php $this->helpers->isActiveMenu("services"); ?>><a href="services#content">SERVICES</a></li>
+			                <li <?php $this->helpers->isActiveMenu("gallery"); ?>><a href="gallery#content">GALLERY</a></li>
+			                <li <?php $this->helpers->isActiveMenu("reviews"); ?>><a href="reviews#content">REVIEWS</a></li>
+			                <li <?php $this->helpers->isActiveMenu("contact"); ?>><a href="contact#content">CONTACT US</a></li>
+						</ul>
+					</nav>
+					<!-- <a class="button" href="#">HOME</a>
 					<a href="#">ABOUT US</a>
 					<a href="#">SERVICES</a>
 					<a href="#">GALLERY</a>
 					<a href="#">CONTACT US</a>
+					<nav>
+						<ul>
+							<li></li>
+						</ul>
+					</nav> -->
 				</div>
 			</div>
 		</div>
 	</header>
 	<!-- Hero -->
 	<?php if($view == "home"):?>
-		<section id="section_2">
-			<div class="section_2_content">
-				<h1>Make Garcia Painting your one and only choice.</h1>
-				<p>NO JOB TOO SMALL! WE DO WHAT THE OTHER GUYS WON’T!</p>
-				<div class="section_2_btn_container">
-					<a href="#">
-						<img src="./public/images/icons/phone-black.fw.png" alt="">
-						<div class="section_2_btn">
-							<small>CALL US TODAY</small>
-							<p>(408) 413-9093</p>
-						</div>
-					</a>
-				</div>
-			</div>
-		</section>
+		
 	<?php endif; ?>

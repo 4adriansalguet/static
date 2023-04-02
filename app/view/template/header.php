3453fc7aeddc03bba221a29eef9b271db0f71e7a
vencerConsul <a.adriansalguet3.code@gmail.com>
2023-04-03 01:25:06 +0800

    small changes

--- app/view/template/header.php
+++ app/view/template/header.php
@@ -1,25 +1,25 @@
-<?php $this->suspensionRedirect($view); ?> <!-- bawal -->
+<?php $this->suspensionRedirect($view); ?>
 <!DOCTYPE html>
-<html lang="en" <?php $this->helpers->htmlClasses(); ?>> <!-- bawal -->
+<html lang="en" <?php $this->helpers->htmlClasses(); ?>>
 <head>
-	<meta charset="utf-8"> <!-- bawal -->
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/> <!-- bawal -->
-	<meta name="apple-mobile-web-app-capable" content="yes" /> <!-- bawal --> 
-	<meta name="apple-mobile-web-app-status-bar-style" content="black" /> <!-- bawal --> 
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/> 
+	<meta name="apple-mobile-web-app-capable" content="yes" /> 
+	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
-	<?php $this->helpers->seo($view); ?> <!-- bawal -->
-	<link rel="icon" href="public/images/favicon.png" type="image/x-icon"> <!-- bawal -->
-	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet"> <!-- bawal -->
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" /> <!-- bawal -->
-	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" /> <!-- bawal -->
+	<?php $this->helpers->seo($view); ?> 
+	<link rel="icon" href="public/images/favicon.png" type="image/x-icon"> 
+	<link href="<?php echo URL; ?>public/styles/style.css" rel="stylesheet"> 
+	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+	<link rel="stylesheet" href="<?php echo URL; ?>public/fancybox/source/jquery.fancybox.css" media="screen" />
 	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
-	<?php $this->helpers->analytics(); ?><!-- bawal -->
+	<?php $this->helpers->analytics(); ?>
 </head>
 
-<body <?php $this->helpers->bodyClasses($view); ?>> <!-- bawal -->
-<?php $this->checkSuspensionHeader(); ?> <!-- bawal -->
+<body <?php $this->helpers->bodyClasses($view); ?>> 
+<?php $this->checkSuspensionHeader(); ?> 
 
 	<header>
 		<div id="header">
@@ -40,7 +40,7 @@
 		<div id="menu">
 			<div class="menu-main">
 				<div class="menu-content">
-					<img src="./public/images/icons/logo.fw.png" alt="img">
+					<img src="./public/images/icons/logo.fw.png" alt="img" onclick="window.location.href='<?php echo URL; ?>'">
 					<nav>
 						<ul>
 			                <li <?php $this->helpers->isActiveMenu("home"); ?> class="btn"><a  href="home#content">HOME</a></li>
--- public/scripts/responsive-menu.js
+++ public/scripts/responsive-menu.js
@@ -1,6 +1,6 @@
 window.onload = function () {
 	window.addEventListener('scroll', function (e) {
-		if (window.pageYOffset > 0) {
+		if (window.pageYOffset > 100) {
 			document.querySelector("header").classList.add('is-scrolling');
 		} else {
 			document.querySelector("header").classList.remove('is-scrolling');
